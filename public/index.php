ff34019961c9eac5bbc9ed04ca837102e7746a1f
gregorysysoev <gregorysysoev@gmail.com>
2020-08-10 22:03:55 +1000

    add form

--- public/index.php
+++ public/index.php
@@ -1,2 +1,18 @@
 <?
-echo 'Hello world!';
+
+$defaultLogin = 'user';
+$defaultPassword = '234567gwqwe';
+
+if ($_POST['log_in']) {
+    $login = $_POST['login']; 
+    $password = $_POST['password'];
+
+    if ($login !== $defaultLogin || $password !== $defaultPassword) 
+    {
+        $message = 'Неверные данные';
+        echo "<script type='text/javascript'>alert('$message');</script>";
+        exit;
+    }
+}
+
+include('index.html');
